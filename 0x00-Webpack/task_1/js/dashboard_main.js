782551bf5a2bf8efb50e1b6018a9d917b36f63ba
L eye <leyume@users.noreply.github.com>
2023-07-24 19:47:36 +0100

    Update dashboard_main.js

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -5,13 +5,13 @@ import _ from 'lodash';
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-//$('body').append("<p id='count'><p>");
+$('body').append("<p id='count'><p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
 var count = 0;
 
 function updateCounter() {
-  count++;
+  ++count;
   $("#count").html(`${count} clicks on the button`);
 };
 
