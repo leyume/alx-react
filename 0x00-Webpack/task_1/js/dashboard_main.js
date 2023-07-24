dc04d1716803c27404b61faf614a394c2dd00bef
L eye <leyume@yahoo.com>
2023-07-24 20:10:31 +0100

    Project: Webpack

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,18 +1,18 @@
 // Import jQuery
-import $ from 'jquery';
-import _ from 'lodash';
+import $ from "jquery";
+import _ from "lodash";
 
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
-$('body').append('<p>Copyright - Holberton School</p>');
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append("<button>Click here to get started</button>");
+$("body").append("<p id='count'></p>");
+$("body").append("<p>Copyright - Holberton School</p>");
 
 var count = 0;
 
 function updateCounter() {
   count++;
-  $('#count').text(count + ' clicks on the button');
+  $("#count").text(count + " clicks on the button");
 }
 
-$('button').on('click', _.debounce(updateCounter));
+$("button").on("click", _.debounce(updateCounter, 100));
